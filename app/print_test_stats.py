24198fc647b2bf64776be38b93e6c99ee8d4b113
JoJoPuppe <marcusloeper@gmx.de>
2020-04-30 09:04:08 +0200

    remove test scripts

--- app/print_test_stats.py
+++
@@ -1,9 +0,0 @@
-from get_warzone_stats import WarzoneStats
-import json
-
-data_obj = WarzoneStats("dlt_orko")
-data_obj.collect_data()
-json_data = data_obj.player_stats
-
-print(json.dumps(json_data,indent=4, sort_keys=True))
-
