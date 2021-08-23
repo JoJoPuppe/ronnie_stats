4a65b956458282dea5fbafb74efe184012c265ba
jojopuppe <marcusloeper@gmx.de>
2021-08-23 21:45:09 +0200

    fixed authentification

--- app/authentication.py
+++ app/authentication.py
@@ -21,54 +21,53 @@ class Authentication(object):
         self.base_path = os.path.dirname(os.path.abspath(__file__))
         self.cookie_file = os.path.join(self.base_path, 'auth_cookies.json')
         self.new_cookie_file = os.path.join(self.base_path, 'new_auth_cookies.json')
-        self._cookies = {}
+        self.cookies = {}
+    
 
-
-    def merge_cookies(self):
+    def update_cookies(self):
+        updated_cookies = []
         new_cookies = self.load_cookies(self.new_cookie_file)
         current_cookies = self.load_cookies(self.cookie_file)
-        updated_cookies = []
-        if current_cookies:
+        write_new_cookie_file = False
+
+        if new_cookies['cookies']:
+            write_new_cookie_file = True
+            updated_cookies.extend(new_cookies['cookies'])
+        
+        if current_cookies['cookies']:
             for cookie in current_cookies['cookies']:
-                if cookie['fails'] >= 5:
+                if cookie['fails'] >= 8:
+                    write_new_cookie_file = True
                     continue
                 updated_cookies.append(cookie)
 
-        if new_cookies:
-            updated_cookies.extend(new_cookies['cookies'])
 
         if len(updated_cookies) <= 0:
-            updater.bot.send_message(chat_id=WARZONE_CONFIG['TELEGRAM_CHAT_ID'], text="no working cookies left. please add more cookies.")
+            updater.bot.send_message(chat_id=WARZONE_CONFIG['TELEGRAM_CHAT_ID'],\
+                    text="no working cookies left. please add more cookies.")
+        else:
+            self.cookies = {"cookies": updated_cookies}
+            if write_new_cookie_file:
+                with open(self.cookie_file, "w+") as f:
+                    json.dump(self.cookies, f)
 
-        cookie_dict = {"cookies": updated_cookies}
-
-        with open(self.cookie_file, "w+") as f:
-            json.dump(cookie_dict, f)
-
-        try:
-            os.remove(self.new_cookie_file)
-        except:
-            print(f"{self.new_cookie_file}: does not exist")
+            try:
+                pass
+                #os.remove(self.new_cookie_file)
+            except:
+                print(f"{self.new_cookie_file}: does not exist")
 
 
     def load_cookies(self, path_to_file) -> dict:
         cookie_dict = {}
         try:
             with open(path_to_file, "r") as f:
-                if len(f.read()) < 1:
-                    return {}
                 cookie_dict = json.load(f)
                 return cookie_dict
 
         except IOError:
             print("File does not exist")
             return {}
-
-
-    def cookies(self):
-        self._cookies = self.load_cookies(self.cookie_file)
-        return self._cookies
-
 
 
     def cookies_failed(self, index: int):
--- update_match_table.py
+++ update_match_table.py
@@ -16,98 +16,101 @@ LOG_FILE = WARZONE_CONFIG['LOGFILE']
 logging.basicConfig(level=logging.INFO, filename=LOG_FILE, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 
 auth = Authentication()
-auth.merge_cookies()
-cookie_dict = auth.cookies()
+auth.update_cookies()
+cookie_dict = auth.cookies
 
-cookies_working = False
+if not cookie_dict:
+    print('no cookies available, please add more cookies')
+else:
+    cookies_working = False
 
-for c in range(len(cookie_dict['cookies'])):
-    if cookies_working:
-        break
-    for name in NAMES:
-        get_stat_obj = WarzoneStats(name, cookie_dict['cookies'][c])
-        match_data = get_stat_obj.request_match_data()
-        if match_data == None:
-            logging.error(f"cant get match data: {name}")
-            print(f"cant get macht data: {name}")
-            auth.cookies_failed(c)
+    for c in range(len(cookie_dict['cookies'])):
+        if cookies_working:
             break
-        else:
-            cookies_working = True
-            jdata = get_stat_obj.collect_match_data(match_data)
-            cnt = 0
+        for name in NAMES:
+            get_stat_obj = WarzoneStats(name, cookie_dict['cookies'][c])
+            match_data = get_stat_obj.request_match_data()
+            if match_data == None:
+                logging.error(f"cant get match data: {name}")
+                print(f"cant get macht data: {name}")
+                auth.cookies_failed(c)
+                break
+            else:
+                cookies_working = True
+                jdata = get_stat_obj.collect_match_data(match_data)
+                cnt = 0
 
-            for m in jdata:
+                for m in jdata:
 
-                matchid = str(m['MatchStat']['matchID'])
+                    matchid = str(m['MatchStat']['matchID'])
 
-                converted_playername = re.sub(r'^\[.*\]','', m["MatchStat"]['playername']).lower()
-                if(converted_playername == 'br3mmel'):
-                    converted_playername = 'br3mm3l'
-                if(converted_playername == 'camarleng0'):
-                    converted_playername = 'camarlengo'
+                    converted_playername = re.sub(r'^\[.*\]','', m["MatchStat"]['playername']).lower()
+                    if(converted_playername == 'br3mmel'):
+                        converted_playername = 'br3mm3l'
+                    if(converted_playername == 'camarleng0'):
+                        converted_playername = 'camarlengo'
 
-                q = MatchStats.query.filter(and_(MatchStats.matchID == matchid,
-                                                    MatchStats.playername == converted_playername)).first()
-                if q == None:
-                    epoch = m['MatchStat']['utcStartSeconds']
-                    start_match_time = time.strftime("%a, %d%b%Y %H:%M:%S", time.localtime(epoch))
-                    logging.info(f'match {start_match_time} of {name} added')
-                else:
-                    #print(f"match from {name} already in db")
-                    continue
+                    q = MatchStats.query.filter(and_(MatchStats.matchID == matchid,
+                                                        MatchStats.playername == converted_playername)).first()
+                    if q == None:
+                        epoch = m['MatchStat']['utcStartSeconds']
+                        start_match_time = time.strftime("%a, %d%b%Y %H:%M:%S", time.localtime(epoch))
+                        logging.info(f'match {start_match_time} of {name} added')
+                    else:
+                        #print(f"match from {name} already in db")
+                        continue
 
-                record = MatchStats(
-                            utcStartSeconds = int(m["MatchStat"]['utcStartSeconds']),
-                            utcEndSeconds = int(m["MatchStat"]['utcEndSeconds']),
-                            matchID = str(m["MatchStat"]['matchID']),
-                            duration = int(m["MatchStat"]['duration']),
-                            playerCount = int(m["MatchStat"]['playerCount']),
-                            teamCount = int(m["MatchStat"]['teamCount']),
-                            playername = converted_playername,
-                            gameMode = m["MatchStat"]['gameMode'],
-                            kills = int(m['MatchPlayerStats']['kills']),
-                            medalXp = int(m['MatchPlayerStats']['medalXp']),
-                            matchXp = int(m['MatchPlayerStats']['matchXp']),
-                            scoreXp = int(m['MatchPlayerStats']['scoreXp']),
-                            score = int(m['MatchPlayerStats']['score']),
-                            totalXp = int(m['MatchPlayerStats']['totalXp']),
-                            headshots = int(m['MatchPlayerStats']['headshots']),
-                            assists = int(m['MatchPlayerStats']['assists']),
-                            challengeXp = int(m['MatchPlayerStats']['challengeXp']),
-                            scorePerMinute = int(m['MatchPlayerStats']['scorePerMinute']),
-                            distanceTraveled = int(m['MatchPlayerStats']['distanceTraveled']),
-                            teamSurvivalTime = int(m['MatchPlayerStats']['teamSurvivalTime']),
-                            deaths = int(m['MatchPlayerStats']['deaths']),
-                            kdRatio = m['MatchPlayerStats']['kdRatio'],
-                            pickupTablet = int(m['MatchPlayerStats']['objectiveBrMissionPickupTablet']),
-                            teamWipes = int(m['MatchPlayerStats']['objectiveTeamWiped']),
-                            boxesOpen = int(m['MatchPlayerStats']['objectiveBrCacheOpen']),
-                            lastStandKills = int(m['MatchPlayerStats']['objectiveLastStandKill']),
-                            shopping = int(m['MatchPlayerStats']['objectiveBrKioskBuy']),
-                            bonusXp = int(m['MatchPlayerStats']['bonusXp']),
-                            circle1 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle1']),
-                            circle2 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle2']),
-                            circle3 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle3']),
-                            circle4 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle4']),
-                            circle5 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle5']),
-                            circle6 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle6']),
-                            circle7 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle7']),
-                            revives = int(m['MatchPlayerStats']['objectiveReviver']),
-                            timePlayed = int(m['MatchPlayerStats']['timePlayed']),
-                            percentTimeMoving = int(m['MatchPlayerStats']['percentTimeMoving']),
-                            miscXp = int(m['MatchPlayerStats']['miscXp']),
-                            longestStreak = int(m['MatchPlayerStats']['longestStreak']),
-                            teamPlacement = int(m['MatchPlayerStats']['teamPlacement']),
-                            damageDone = int(m['MatchPlayerStats']['damageDone']),
-                            damageTaken = int(m['MatchPlayerStats']['damageTaken']))
+                    record = MatchStats(
+                                utcStartSeconds = int(m["MatchStat"]['utcStartSeconds']),
+                                utcEndSeconds = int(m["MatchStat"]['utcEndSeconds']),
+                                matchID = str(m["MatchStat"]['matchID']),
+                                duration = int(m["MatchStat"]['duration']),
+                                playerCount = int(m["MatchStat"]['playerCount']),
+                                teamCount = int(m["MatchStat"]['teamCount']),
+                                playername = converted_playername,
+                                gameMode = m["MatchStat"]['gameMode'],
+                                kills = int(m['MatchPlayerStats']['kills']),
+                                medalXp = int(m['MatchPlayerStats']['medalXp']),
+                                matchXp = int(m['MatchPlayerStats']['matchXp']),
+                                scoreXp = int(m['MatchPlayerStats']['scoreXp']),
+                                score = int(m['MatchPlayerStats']['score']),
+                                totalXp = int(m['MatchPlayerStats']['totalXp']),
+                                headshots = int(m['MatchPlayerStats']['headshots']),
+                                assists = int(m['MatchPlayerStats']['assists']),
+                                challengeXp = int(m['MatchPlayerStats']['challengeXp']),
+                                scorePerMinute = int(m['MatchPlayerStats']['scorePerMinute']),
+                                distanceTraveled = int(m['MatchPlayerStats']['distanceTraveled']),
+                                teamSurvivalTime = int(m['MatchPlayerStats']['teamSurvivalTime']),
+                                deaths = int(m['MatchPlayerStats']['deaths']),
+                                kdRatio = m['MatchPlayerStats']['kdRatio'],
+                                pickupTablet = int(m['MatchPlayerStats']['objectiveBrMissionPickupTablet']),
+                                teamWipes = int(m['MatchPlayerStats']['objectiveTeamWiped']),
+                                boxesOpen = int(m['MatchPlayerStats']['objectiveBrCacheOpen']),
+                                lastStandKills = int(m['MatchPlayerStats']['objectiveLastStandKill']),
+                                shopping = int(m['MatchPlayerStats']['objectiveBrKioskBuy']),
+                                bonusXp = int(m['MatchPlayerStats']['bonusXp']),
+                                circle1 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle1']),
+                                circle2 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle2']),
+                                circle3 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle3']),
+                                circle4 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle4']),
+                                circle5 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle5']),
+                                circle6 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle6']),
+                                circle7 = int(m['MatchPlayerStats']['objectiveBrDownEnemyCircle7']),
+                                revives = int(m['MatchPlayerStats']['objectiveReviver']),
+                                timePlayed = int(m['MatchPlayerStats']['timePlayed']),
+                                percentTimeMoving = int(m['MatchPlayerStats']['percentTimeMoving']),
+                                miscXp = int(m['MatchPlayerStats']['miscXp']),
+                                longestStreak = int(m['MatchPlayerStats']['longestStreak']),
+                                teamPlacement = int(m['MatchPlayerStats']['teamPlacement']),
+                                damageDone = int(m['MatchPlayerStats']['damageDone']),
+                                damageTaken = int(m['MatchPlayerStats']['damageTaken']))
 
-                db.session.add(record)
-                cnt += 1
-                #print(f"match added {name}")
+                    db.session.add(record)
+                    cnt += 1
+                    #print(f"match added {name}")
 
-        print(f'{cnt} matches of {name} added. wait 3s')
-        logging.info(f'{cnt} matches of {name} added. wait 3s')
-        time.sleep(3)
+            print(f'{cnt} matches of {name} added. wait 3s')
+            logging.info(f'{cnt} matches of {name} added. wait 3s')
+            time.sleep(3)
 
-db.session.commit()
+    db.session.commit()
